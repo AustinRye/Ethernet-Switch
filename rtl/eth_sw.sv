ae47c66a755dde2d775aeba42b33c4c3bf7a7fe0
AustinWRye <RyeAustinW@gmail.com>
2020-08-22 18:29:53 -0400

    Add ethernet transmitter

--- rtl/eth_sw.sv
+++ rtl/eth_sw.sv
@@ -6,17 +6,21 @@
 // Ethernet Switch
 ////////////////////////////////////////////////////////////////////////////////
 
+`include "fifo.sv"
+`include "eth_rx.sv"
+`include "eth_tx.sv"
+
 module eth_sw
 	(
-        input  logic        clk,           // clock
-        input  logic        rstn,          // reset active low
-        input  logic [31:0] i_data  [0:1], // Port input data
-        input  logic        i_start [0:1], // Start of input packet data
-        input  logic        i_end   [0:1], // End of input packet data
-        output logic [31:0] o_data  [0:1], // Port output data
-        output logic        o_start [0:1], // Start of output packet data
-        output logic        o_end   [0:1], // End of output packet data
-        output logic        stall   [0:1]  // Port backpressure/stall signal
+        input  logic        clk,              // clock
+        input  logic        rstn,             // reset active low
+        input  logic [31:0] i_data     [0:1], // Port input data
+        input  logic        i_start    [0:1], // Start of input packet data
+        input  logic        i_end      [0:1], // End of input packet data
+        output logic [31:0] o_data     [0:1], // Port output data
+        output logic        o_start    [0:1], // Start of output packet data
+        output logic        o_end      [0:1], // End of output packet data
+        output logic        port_stall [0:1]  // Port backpressure/stall signal
     );
   
     logic        fifo_wr_en   [0:1]; // fifo write enable
@@ -25,6 +29,8 @@ module eth_sw
     logic [33:0] fifo_rd_data [0:1]; // fifo read data {o_data[31:0], o_start, o_end}
     logic        fifo_empty   [0:1]; // fifo empty flag
     logic        fifo_full    [0:1]; // fifo full flag
+  
+    logic        port_busy    [0:1]; // port busy flag
 
     fifo #(
         .WIDTH (34),
@@ -73,5 +79,19 @@ module eth_sw
         .wr_data (fifo_wr_data[1]),
         .wr_en   (fifo_wr_en[1])
     );
-
+  
+    eth_tx u_eth_tx (
+        .clk       (clk),
+        .rstn      (rstn),
+        .rd_data   (fifo_rd_data),
+        .empty     (fifo_empty),
+        .full      (fifo_full),
+        .i_port_busy (port_busy),
+        .rd_en     (fifo_rd_en),
+        .o_data    (o_data),
+        .o_start   (o_start),
+        .o_end     (o_end),
+        .o_port_busy (port_busy)
+    );
+  
 endmodule
---
+++ rtl/eth_tx.sv
@@ -0,0 +1,126 @@
+////////////////////////////////////////////////////////////////////////////////
+// Author: Austin Rye <ryeaustinw@gmail.com>
+//
+// Name: eth_tx
+// Description:
+// Ethernet Transmitter that sends out data from the FIFO queue
+////////////////////////////////////////////////////////////////////////////////
+
+module eth_tx
+    #(
+      parameter [31:0] PORT_ADDR [0:1] = {0, 0}
+    ) (
+ 		input  logic        clk,             // clock
+      	input  logic        rstn,            // reset active low
+        input  logic [33:0] rd_data [0:1],         // port input data
+        input  logic        empty [0:1],           // queue empty flag
+      	input  logic        full [0:1],            // queue full flag
+        input  logic        i_port_busy [0:1], // port busy flag
+      	output logic        rd_en [0:1],           // start of input packet data
+        output logic [31:0] o_data    [0:1], // end of input packet data
+        output logic        o_start   [0:1], // write data {i_data[31:0], i_start, i_end}
+        output logic        o_end     [0:1],  // write enable
+        output logic        o_port_busy [0:1] // port busy flag
+    );
+  
+    typedef enum logic [1:0] {IDLE, DEST_ADDR, TX} tx_states;
+    tx_states tx_state [0:1];
+    tx_states tx_next_state [0:1];
+  
+    logic        dest_port [0:1];
+  
+    logic [31:0] i_data [0:1];
+    logic        i_start [0:1];
+    logic        i_end [0:1];
+  
+  	integer i;
+  	
+  	generate
+    for (genvar i=0; i<=1; i++)
+    begin
+  
+        assign i_data[i]  = rd_data[i][31:0];
+        assign i_start[i] = rd_data[i][32];
+        assign i_end[i]   = rd_data[i][33];
+
+        always @(posedge clk or negedge rstn)
+        begin
+            if (!rstn)
+                tx_state[i] <= IDLE;
+            else
+                tx_state[i] <= tx_next_state[i];
+        end
+
+        always @(tx_state[i])
+        begin
+            case(tx_state[i])
+                IDLE: begin
+                    if (~empty[i])
+                        rd_en[i] <= 1;
+                end
+                DEST_ADDR: begin
+                    if (i_start[i])
+                    begin
+                        for (int i=0; i<=1; i++)
+                        begin
+                            if (i_data[i] == PORT_ADDR[i])
+                                dest_port[i] = i;
+                        end
+                        if (i_port_busy[dest_port[i]])
+                            rd_en[i] <= 0;
+                        else
+                        begin
+                            o_port_busy[dest_port[i]] <= 1;	
+                            rd_en[i] <= 1;
+                        end
+                    end
+                end
+                TX: begin
+                    if (i_end[i])
+                    begin
+                        o_port_busy[dest_port[i]] <= 0;
+                        rd_en[i] <= 0;
+                    end
+                end
+                default: begin
+                    dest_port[i] <= 0;
+                end
+            endcase
+        end
+
+        always @(tx_state[i])
+        begin
+            case(tx_state[i])
+                IDLE: begin
+                  if (~empty[i])
+                        tx_next_state[i] <= DEST_ADDR;
+                    else
+                        tx_next_state[i] <= IDLE;
+                end
+                DEST_ADDR: begin
+                    if (i_start[i])
+                      if (i_port_busy[dest_port[i]])
+                            tx_next_state[i] <= DEST_ADDR;
+                        else
+                        begin
+                            tx_next_state[i] <= TX;
+                        end
+                    else
+                        tx_next_state[i] <= DEST_ADDR; 
+                end
+                TX: begin
+                    if (i_end[i])
+                        tx_next_state[i] <= IDLE;
+                    else
+                        tx_next_state[i] <= TX;
+                end
+                default: begin
+                    tx_next_state[i] <= IDLE; 
+                end
+            endcase
+        end
+      
+    end
+    endgenerate
+
+endmodule
